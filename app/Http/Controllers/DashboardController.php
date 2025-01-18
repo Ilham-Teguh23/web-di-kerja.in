8596a44d0c1fd18661ab880bcec588df0004f89c
Ilham-Teguh23 <mohammadteguhriyadi23@gmail.com>
2025-01-19 02:06:15 +0700

    Modified: Move Function

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         return view('dashboard.dashboard'); // View untuk form login
+    }
+
+    public function authLogout()
+    {
+        Auth::logout();
+
+        return redirect()->route("login")->with("success", "Logout Berhasil");
     }
 }

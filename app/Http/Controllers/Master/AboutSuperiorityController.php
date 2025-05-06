fbab353fd859385ac8d9647f9f1e8dd2fd7c8bad
Siinchi <dimasvorx117@gmail.com>
2025-05-06 07:26:42 +0700

    AboutSuperiority

--- app/Http/Controllers/Master/AboutSuperiorityController.php
+++ app/Http/Controllers/Master/AboutSuperiorityController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers\Master;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ListSuperiority;
+use Yajra\DataTables\DataTables;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Models\AboutListSuperiority;
 
 class AboutSuperiorityController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    protected $listSuperiority;
+
+    public function __construct()
+    {
+        $this->listSuperiority = new ListSuperiority();
+    }
+
     public function index()
     {
-        //
+        return view("dashboard.meta-data.list-superiority.index");
     }
 
     /**
@@ -28,15 +36,61 @@ class AboutSuperiorityController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+
+            DB::beginTransaction();
+
+            $this->listSuperiority->create([
+                "name" => $request->name,
+            ]);
+
+            DB::commit();
+
+            return response()->json([
+                "status" => true,
+                "message" => "Data Berhasil di Tambah"
+            ]);
+
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage()
+            ]);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        // dd($id);
+        try {
+
+            DB::beginTransaction();
+
+            $data = $this->listSuperiority->where("id", $id)->first();
+
+            DB::commit();
+
+            return response()->json([
+                "status" => true,
+                "message" => "Data Showed By ID Successfully",
+                "data" => $data
+            ]);
+
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage()
+            ]);
+        }
     }
 
     /**
@@ -50,16 +104,67 @@ class AboutSuperiorityController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+
+            DB::beginTransaction();
+
+            $this->listSuperiority->where("id", $id)->update([
+                "name" => $request->name,
+            ]);
+
+            DB::commit();
+
+            return response()->json([
+                "status" => true,
+                "message" => "Update Data Success"
+            ]);
+
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage()
+            ]);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        try {
+
+            DB::beginTransaction();
+
+            $this->listSuperiority->where("id", $id)->delete();
+
+            DB::commit();
+
+            return response()->json([
+                "status" => true,
+                "message" => "Delete Data Success"
+            ]);
+
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
+            return response()->json([
+                "status" => false,
+                "message" => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function datatable(Request $request)
+    {
+        $data = ListSuperiority::orderBy('created_at')->get();
+
+        return DataTables::of($data)->make();
     }
 }
